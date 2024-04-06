ca3d2b2fddc66ab1bdb3fc188b3ae031980083f5
TomGPou <tomas.gomezpou@gmail.com>
2024-04-06 16:23:13 -0300

    se agrega utilizavion de fs y metodos update y delete

--- productManager.js
+++ productManager.js
@@ -1,9 +1,25 @@
+import fs from "fs";
+
 class ProductManager {
   constructor() {
     this.products = [];
+    this.path = "./products.json";
   }
 
-  addProduct(title, description, price, thumbnail, code, stock) {
+  async readFile() {
+    const data = await fs.promises.readFile(this.path);
+    this.products = JSON.parse(data);
+  }
+
+  async writeFile() {
+    await fs.promises.writeFile(
+      this.path,
+      JSON.stringify(this.products),
+      "utf-8"
+    );
+  }
+
+  async addProduct(title, description, price, thumbnail, code, stock) {
     // Validar la carga de datos
     if (!title || !description || !price || !thumbnail || !code || !stock)
       throw new Error("Falta completar datos del producto");
@@ -24,23 +40,65 @@ class ProductManager {
       code,
       stock,
     };
-    this.products.push(newProduct);
 
+    this.products.push(newProduct);
+    await this.writeFile();
     return newProduct;
   }
 
   // Obtener lista de productos
-  getProducts() {
+  async getProducts() {
+    if (!this.products.length) await this.readFile();
     return this.products;
   }
 
   //  Buscar un producto por su id
-  getProductById(id) {
-    const product = this.products.find((p) => p.id == id);
+  async getProductById(id) {
+    const productsList = await this.getProducts();
+    const product = productsList.find((p) => p.id === id);
     if (!product) {
       throw new Error("NOT FOUND");
     }
     return product;
+  }
+
+  // Actualizar producto
+  async updateProduct(id, data) {
+    const productsList = await this.getProducts();
+
+    const i = productsList.findIndex((item) => item.id === id);
+    // verificar que exista el ID
+    if (i < 0) {
+      throw new Error(`NOT FOUND`);
+    } else {
+      // verificar que no elimine el ID
+      if ("id" in data) {
+        delete data.id;
+      }
+      // modificar producto y actualizar DB
+      this.products[i] = { ...this.products[i], ...data };
+
+      await this.writeFile();
+      return this.products[i];
+    }
+  }
+
+  // Borrar producto
+  async deleteProduct(id) {
+    const productsList = await this.getProducts();
+
+    const i = productsList.findIndex((item) => item.id === id);
+    // verificar que exista el ID
+    if (i < 0) {
+      throw new Error(`NOT FOUND`);
+    } else {
+      // Quitar producto del array y actualizar DB
+      productsList.splice(i, 1);
+      this.products = [...productsList];
+
+      await this.writeFile();
+      return console.log("Producto eliminado");
+    }
   }
 }
 
@@ -67,8 +125,25 @@ productManager.addProduct(
 );
 
 // ver productos
-const productsList = productManager.getProducts();
-console.log(productsList);
+// const productsList = productManager.getProducts();
+// console.log(productsList);
 
 //  buscar producto por ID
-// const productId = productManager.getProductById(1);
+// const productId = await productManager.getProductById(1);
+// console.log(productId);
+
+// actualizar un producto
+// const updatedProduct = await productManager.updateProduct(2, {
+//   title: "Producto actualizado",
+//   price: 300,
+// });
+// console.log(updatedProduct);
+
+// const productsList = productManager.getProducts();
+// console.log(productsList);
+
+// borrar producto
+// await productManager.deleteProduct(1);
+
+// const productsList = productManager.getProducts();
+// console.log(productsList);
