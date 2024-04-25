c8c1517e7618336d70e942066bef8172a253a2ff
TomGPou <tomas.gomezpou@gmail.com>
2024-04-25 11:49:47 -0300

    se renderiza la lista de productos

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
+import ProductManager from "../productManager.js";
 
 //* INIT
 const router = Router();
+const productManager = new ProductManager();
 
 //* ENDPOINTS
 // Lista de productos
-router.get("/products", (req, res) => {
-  res.render("index");
+router.get("/products", async (req, res) => {
+  const products = { products: await productManager.getProducts() };
+  res.render("index", products);
 });
 
 export default router;
