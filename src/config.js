51e856339f50f6a63dad1d565b319134ec3bfb77
TomGPou <tomas.gomezpou@gmail.com>
2024-06-10 11:49:45 -0300

    se modifica config

--- src/config.js
+++ src/config.js
@@ -8,11 +8,11 @@ const config = {
   },
   MONGODB_URI:
     "mongodb+srv://admin:coder2024@clusterproyecto.09d4t4t.mongodb.net/ecommerce",
-  SECRET: 'coder_53160_abc',
+  SECRET: "coder_53160_abc",
 
-  GITHUB_CLIENT_ID: 'Iv23liB8jgKFdThwsuEg',
-  GITHUB_CLIENT_SECRET: '2042d962897109a0cd6edc2d4160d86233558609',
-  GITHUB_CALLBACK_URL: 'http://localhost:5050/api/session/ghlogincallback'
+  GITHUB_CLIENT_ID: "Iv23liB8jgKFdThwsuEg",
+  GITHUB_CLIENT_SECRET: "2042d962897109a0cd6edc2d4160d86233558609",
+  GITHUB_CALLBACK_URL: "http://localhost:5050/api/auth/ghlogincallback",
 };
 
 export default config;
